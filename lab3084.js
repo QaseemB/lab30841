80e54420fa7198f6e9ef0bffd81a519fdf3107fd
QaseemB <Qbarnhardt@yahoo.com>
2024-05-27 19:44:15 -0400

    lab30841 pt2

---
+++ lab3084.js
@@ -0,0 +1,47 @@
+//                                 Part 1: Refactoring Old Code
+// When code is outdated or inefficient, it often goes through a process called “refactoring.” Refactoring code is the process of restructuring that code without changing its original behavior.
+// For the first part of this assignment, revisit your code from ALAB 308.3.1, wherein you create a script that parsed CSVs. Now that you have knowledge of arrays and objects, how would you change your approach to this problem? Take a few minutes to examine and refactor the code before continuing.
+// For reference, ALAB 308.3.1 is embedded below. The section on CSV parsing is “Part 3.”
+
+
+                                // the old code from the last lab 
+
+// let CSV = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26';
+// let CSVwithout_commas = CSV.replaceAll(',', ' '); 
+
+// let rows = CSVwithout_commas.split('\n');  
+
+// for (let c of rows) {
+//     let cells = c.split(' ');
+//         let cell1 = cells[0];
+//         let cell2 = cells[1];
+//         let cell3 = cells[2];
+//         let cell4 = cells[3];
+//         console.log(cell1,cell2,cell3,cell4); 
+// }
+
+
+
+// Part 2: Expanding Functionality
+// Now that you are familiar with your code, and perhaps have improved it, it is time to expand upon its functionality.
+// Begin with the following task:
+// Declare a variable that stores the number of columns in each row of data within the CSV.
+// Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+// For example, if the first row of data (the headings) has eight entries, your program should create eight entries per row. You can safely assume that all rows that follow will contain the same number of entries per row.
+// After you have implemented the above:
+// Store your results in a two-dimensional array.
+// Each row should be its own array, with individual entries for each column.
+// Each row should be stored in a parent array, with the heading row located at index 0.
+// Cache this two-dimensional array in a variable for later use.
+// Using the original CSV example data, here is what the result of this step should look like:
+// ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+
+
+
+let CSV = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26';
+let CSVwithout_commas = CSV.replaceAll(',', ' '); 
+let rows = CSVwithout_commas.split('\n');  
+let firstRow = rows[0]
+let columns = firstRow.split(',')
+let columnSize = columns.length
+console.log(columns);  
